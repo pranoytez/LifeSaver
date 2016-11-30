0792b38bc8a1dfcb0b67527e11eba6ff96aa4831
pranoy <pranoytez@gmail.com>
2016-11-29 19:16:04 -0600

    Added starting screen

--- app/src/main/java/com/code/pranoy/lifesaver/MainActivity.java
+++ app/src/main/java/com/code/pranoy/lifesaver/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,14 +21,17 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Button enterApp = (Button) findViewById(R.id.enterApp);
+        Button info = (Button) findViewById(R.id.info);
+
+        info.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent i = new Intent(MainActivity.this,AboutActivity.class);
+                        startActivity(i);
+                    }
+                }
+        );
     }
 
     @Override
